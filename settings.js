8e0d88ca945d1acb5a317cb916a69502e30d7f15
napoleonmarcdawhensky-tech <napoleonmarcdawhensky@gmail.com>
2026-03-22 18:00:53 -0400

    Add settings.js for bot configuration and assets

---
+++ settings.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+module.exports = {
+    PREFIX: process.env.PREFIX || '.',
+    BOT_NAME: process.env.BOT_NAME || 'Noxtalis',
+    OWNER_NAME: process.env.OWNER_NAME || 'LordEmeraude',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '50943768238',
+    TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN || '',
+    TELEGRAM_OWNER_ID: process.env.TELEGRAM_OWNER_ID || '',
+    WA_GROUP: process.env.WA_GROUP || 'https://chat.whatsapp.com/C2tTpefkqXIEsVmLbXHd7Y',
+    WA_CHANNEL: process.env.WA_CHANNEL || 'https://whatsapp.com/channel/0029Vb7fvq40LKZCuz02Fi0M',
+    TELEGRAM_OWNER_LINK: process.env.TELEGRAM_OWNER_LINK || 'https://t.me/lordemeraude99',
+
+    IMAGES: {
+        MENU: 'https://files.catbox.moe/ruwxgu.jpg',
+        MAIN_MENU: 'https://files.catbox.moe/sh8dec.jpg',
+        BUG_MENU: 'https://files.catbox.moe/acgsle.jpg',
+        CRASH_SUCCESS: 'https://files.catbox.moe/3n6xba.jpg',
+        TAGALL_1: 'https://files.catbox.moe/w8qsoz.jpg',
+        TAGALL_2: 'https://files.catbox.moe/fq09i3.jpg',
+        TAGALL_3: 'https://files.catbox.moe/8bzykc.jpg',
+        GOODBYE: 'https://files.catbox.moe/3j9xqw.jpg',
+        CRASH_1: 'https://files.catbox.moe/onkpl9.jpg',
+        CRASH_2: 'https://files.catbox.moe/2tlcvg.jpg',
+        CRASH_3: 'https://files.catbox.moe/l8bys5.jpg',
+        CRASH_4: 'https://files.catbox.moe/zqdt9s.jpg',
+        CRASH_5: 'https://files.catbox.moe/wfoxqz.jpg',
+        CRASH_6: 'https://files.catbox.moe/xphio1.jpg',
+        CRASH_7: 'https://files.catbox.moe/88i1l1.jpg',
+        CRASH_8: 'https://files.catbox.moe/r4306v.jpg',
+        MISC_1:  'https://files.catbox.moe/88xtes.jpg',
+    },
+
+    VIDEOS: { INTRO: 'https://files.catbox.moe/iwgzqd.mp4' },
+    SIGNATURE: '🖤 *Dev · LordEmeraude*',
+    JP_QUOTE: '私は楽しみのために殺さない…\n_I don\'t kill for pleasure…_\n我不是为了乐趣而杀戮'
+}
